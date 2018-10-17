79afa87d4635fdd87dd6443f33f4cec38b9cdc21
aanar.p303 <aanar.p303@code.edu.az>
2018-10-17 04:15:30 +0400

    Initial

---
+++ CodeAcademySite/CodeAcademySite/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using CodeAcademySite.Models;
+
+namespace CodeAcademySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //protected List<TopNavbar> topNavbars { get; private set; }
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (CodeAcademyDbEntities db = new CodeAcademyDbEntities())
+            {
+                ViewBag.topNavbars = db.TopNavbars.ToList();
+            }
+
+            return View();
+
+            
+        }
+
+     
+    }
+}
